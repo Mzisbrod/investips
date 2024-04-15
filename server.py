2f692f8d7eedcfd9608b9f8e8946bdd2d71aaebc
Mzisbrod <may_zis@hotmail.com>
2024-04-15 19:25:35 -0400

    Added sidebar

--- server.py
+++ server.py
@@ -3,23 +3,45 @@ from flask import Flask, render_template, request, jsonify
 
 classes_content = {
     'basics': {
-        'title': 'Introduction to HTML',
+        'title': 'Basics',
         'text_content': 'HTML is the standard markup language for creating Web pages.',
         'video_content': 'https://www.example.com/html_intro_video'
     },
     'risk_vs_reward': {
-        'title': 'Introduction to HTML',
+        'title': 'Risk vs. Reward',
         'text_content': 'HTML is the standard markup language for creating Web pages.',
         'video_content': 'https://www.example.com/html_intro_video'
     },
     'compounding': {
-        'title': 'Introduction to CSS',
+        'title': 'Power of Compounding',
         'text_content': 'CSS is a language that describes the style of an HTML document.',
         'video_content': 'https://www.example.com/css_intro_video'
     },
     # Add more classes as needed
 }
 
+quizzes_content = {
+    'basics': {
+        'title': 'Basics',
+        'text_content': 'HTML is the standard markup language for creating Web pages.',
+        'video_content': 'https://www.example.com/html_intro_video'
+    },
+    'risk_vs_reward': {
+        'title': 'Risk vs. Reward',
+        'text_content': 'HTML is the standard markup language for creating Web pages.',
+        'video_content': 'https://www.example.com/html_intro_video'
+    },
+    'compounding': {
+        'title': 'Power of Compounding',
+        'text_content': 'CSS is a language that describes the style of an HTML document.',
+        'video_content': 'https://www.example.com/css_intro_video'
+    },
+    'final': {
+        'title': 'Final Quiz',
+        'text_content': 'CSS is a language that describes the style of an HTML document.',
+        'video_content': 'https://www.example.com/css_intro_video'
+    },
+}
 
 
 app = Flask(__name__)
@@ -33,6 +55,14 @@ def class_info(class_name):
     content = classes_content.get(class_name, None)
     if content:
         return render_template('class.html', content=content)
+    else:
+        return "Class not found", 404
+
+@app.route('/quiz/<quiz_name>')
+def quiz_info(quiz_name):
+    content = quizzes_content.get(quiz_name, None)
+    if content:
+        return render_template('quiz.html', content=content)
     else:
         return "Class not found", 404
 
