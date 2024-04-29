4cd0441d1e6d3e19a3d5cc251fc8c2d2fbc12d7d
Mzisbrod <may_zis@hotmail.com>
2024-04-29 18:31:44 -0400

    Fixed small screen fitting

--- server.py
+++ server.py
@@ -35,7 +35,7 @@ classes_content = {
     },
     'Risk vs. Reward 2': {
         'module': "classes_content",
-        'title': 'Risk vs. Reward',
+        'title': 'Risk vs. Reward (2)',
         'text_content': "Balance your portfolio, so that you don't get wiped out!",
         'media_content': "https://www.youtube.com/embed/O-QwiAGtu88",
         'prev': "Risk vs. Reward",
@@ -58,7 +58,7 @@ classes_content = {
     },
     'Compounding 2': {
         'module': "classes_content",
-        'title': 'Compounding',
+        'title': 'Power of Compounding (2)',
         'text_content': '"The most powerful force in the world is compound interest." - Albert Einstein',
         'media_content': "https://www.youtube.com/embed/V7WLYoQNu80",
         'prev': "Compounding",
